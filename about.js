6000a718281d10b15313f84e7901225f95fb3916
Karthik H Kamath <74257697+KarthikhKamath@users.noreply.github.com>
2021-05-27 22:18:01 +0530

    Add files via upload

---
+++ about.js
@@ -0,0 +1,35 @@
+var cursor = document.querySelector(".cursor")
+var links = document.querySelectorAll(".link")
+var logo = document.querySelector(".logo")
+var about = document.querySelector(".about h2")
+var p = document.querySelector(".about p")
+
+window.addEventListener("mousemove",movement)
+function movement(e){
+    cursor.style.top = e.pageY +"px"
+    cursor.style.left = e.pageX +"px"    
+}
+links.forEach(links => {
+    links.addEventListener("mouseover", function(){
+    cursor.classList.add("grow")
+    })
+     links.addEventListener("mouseleave", function(){
+    cursor.classList.remove("grow")
+    })
+})
+    about.addEventListener("mouseover", function(){
+    cursor.classList.add("grows")
+    
+    })
+     about.addEventListener("mouseleave", function(){
+    cursor.classList.remove("grows")
+    })
+    
+    p.addEventListener("mouseover", function(){
+    cursor.classList.add("grow")
+    
+    })
+     p.addEventListener("mouseleave", function(){
+    cursor.classList.remove("grow")
+    })
+
